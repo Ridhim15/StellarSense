d9ea1a056659e9c2dc5e3c5139eb3ca776c709b3
Ridhim <ridhimg15@gmail.com>
2024-06-03 21:29:33 +0530

    Made unsplash work

--- static/script.js
+++ static/script.js
@@ -5,39 +5,33 @@ function updateSlider() {
 	let i = document.getElementById("slider-i").value
 	let z = document.getElementById("slider-z").value
 	let redshift = document.getElementById("slider-redshift").value
+	let prev_prediction = "GALAXY"
 
 	fetch("backend?u=" + u + "&g=" + g + "&r=" + r + "&i=" + i + "&z=" + z + "&redshift=" + redshift)
 		.then(response => response.json())
 		.then(data => {
-			updateImage(data.prediction)
+			if (data.prediction != prev_prediction) {
+				prev_prediction = data.prediction
+				console.log("Prev_result: " + prev_prediction)
+				updateImage(data.prediction)
+			}
 		})
 }
-function getimage(prediction) {
+function updateImage(prediction) {
+	let conf = (Math.random() * 50 + 50).toFixed(2)
+	let resimg = document.getElementById("resultimg")
+	let restext = document.getElementById("resulttext")
+	restext.innerHTML = `${prediction} with ${conf} %`
+
 	fetch(
 		`https://api.unsplash.com/search/photos?client_id=sYbFMveX6b4wDX6TUfIqtGfYGF31hyOZgEnSF2_yo20&query=${prediction}`
 	)
 		.then(response => response.json())
 		.then(data => {
-			return data.results[0].urls.regular
+			let random = Math.floor(Math.random() * 10)
+			console.log(random)
+			resimg.src = data.results[random].urls.regular
 		})
-}
-function updateImage(prediction) {
-	console.log(prediction)
-	console.log(
-		fetch(
-			`https://api.unsplash.com/search/photos?client_id=sYbFMveX6b4wDX6TUfIqtGfYGF31hyOZgEnSF2_yo20&query=${prediction}`
-		)
-	)
-	let img_num = Math.floor(Math.random() * 20) + 1
-	let img_url = getimage(prediction)
-
-	let conf = (Math.random() * 50 + 50).toFixed(2)
-
-	let resimg = document.getElementById("resultimg")
-	resimg.src = img_url
-	console.log(img_url)
-	let restext = document.getElementById("resulttext")
-	restext.innerHTML = `${prediction} with ${conf} %`
 }
 
 // <link rel="stylesheet" href="{{ url_for('static',filename='style.css') }}">
